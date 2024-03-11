f9b13b1d0827b8d4bade2ba48f2ecc069838416c
trong <lehuutrong141@gmail.com>
2024-03-11 20:47:02 +0700

    add app bar

--- app/src/main/java/com/example/xender/activity/MainActivity.java
+++ app/src/main/java/com/example/xender/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.xender.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -13,11 +14,15 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     NavController navController;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        toolbar = findViewById(R.id.appbar_main);
+        setSupportActionBar(toolbar);
 
         BottomNavigationView navigationView = findViewById(R.id.navigation_view);
         NavigationUI.setupWithNavController(navigationView,navController);
