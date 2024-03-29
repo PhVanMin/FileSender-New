107d7ab17c7cc05ae8f5c0ddb9f4f7fc5bc4342c
21127360 <pvminh21@clc.fitus.edu.vn>
2024-03-29 21:19:06 +0700

    add cancel button to notification

--- app/src/main/java/com/example/xender/activity/MainActivity.java
+++ app/src/main/java/com/example/xender/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.xender.activity;
 
+import static androidx.core.app.NotificationCompat.EXTRA_NOTIFICATION_ID;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -15,6 +17,8 @@ import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -31,10 +35,13 @@ import com.example.xender.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     NavController navController;
     Toolbar toolbar;
-
+    public static boolean downloading = true;
+    int PROGRESS_MAX = 100;
+    int PROGRESS_CURRENT = 0;
+    int PROGRESS_STEP = 5;
+    Thread updateProgress;
     @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,26 +51,49 @@ public class MainActivity extends AppCompatActivity {
         createNotificationChannel();
         Button showDialog = findViewById(R.id.ShowDialog);
         showDialog.setOnClickListener(v -> {
+            Intent snoozeIntent = new Intent(this, MyBroadcastReceiver.class);
+            snoozeIntent.setAction(MyBroadcastReceiver.ACTION_CANCEL);
+            PendingIntent snoozePendingIntent =
+                    PendingIntent.getBroadcast(this, 0, snoozeIntent, PendingIntent.FLAG_IMMUTABLE);
+
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Transfer")
                     .setSmallIcon(R.drawable.send_svgrepo_com)
-                    .setContentTitle("Title")
-                    .setContentText("Context text")
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                    .setContentTitle("Transferring files...")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .addAction(R.drawable.send_svgrepo_com, "Cancel", snoozePendingIntent);
+
+            builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
+            NotificationManagerCompat notification = NotificationManagerCompat.from(this);
+
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 Log.d("Permission", "Failed");
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
                 return;
             }
-            Log.d("Permission", "Success");
-            NotificationManagerCompat notification = NotificationManagerCompat.from(this);
-            notification.notify(1, builder.build());
+
+            updateProgress = new Thread(() -> {
+                notification.notify(1, builder.build());
+
+                while (downloading && PROGRESS_CURRENT < PROGRESS_MAX) {
+                    PROGRESS_CURRENT += PROGRESS_STEP;
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
+                    notification.notify(1, builder.build());
+
+                    if (PROGRESS_CURRENT > PROGRESS_MAX)
+                        PROGRESS_CURRENT = PROGRESS_MAX;
+                }
+
+                builder.setContentText("Download complete")
+                        .setProgress(0,0,false);
+                notification.notify(1, builder.build());
+            });
+
+            updateProgress.start();
         });
 //        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //
