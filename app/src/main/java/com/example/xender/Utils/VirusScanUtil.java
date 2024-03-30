30761221cdb3d7af6c2420b73a35c4255ee42b4c
21127360 <pvminh21@clc.fitus.edu.vn>
2024-03-30 22:10:19 +0700

    complete Virus Scan for app

--- app/src/main/java/com/example/xender/Utils/VirusScanUtil.java
+++ app/src/main/java/com/example/xender/Utils/VirusScanUtil.java
@@ -1,6 +1,10 @@
 package com.example.xender.Utils;
 
+import android.util.Log;
+
+import com.kanishka.virustotal.dto.FileScanReport;
 import com.kanishka.virustotal.dto.ScanInfo;
+import com.kanishka.virustotal.dto.VirusScanInfo;
 import com.kanishka.virustotal.exception.APIKeyNotFoundException;
 import com.kanishka.virustotal.exception.UnauthorizedAccessException;
 import com.kanishka.virustotalv2.VirusTotalConfig;
@@ -9,34 +13,59 @@ import com.kanishka.virustotalv2.VirustotalPublicV2Impl;
 
 import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VirusScanUtil {
-    public static void scanFile(String filePath) {
+    public static String scanFile(File fileToScan) {
         try {
             VirusTotalConfig.getConfigInstance().setVirusTotalAPIKey("4ae0176f319cdb95bc384f4c85b7679d1a28f391e4d47f482a4b67f26a487bc9");
             VirustotalPublicV2 virusTotalRef = new VirustotalPublicV2Impl();
 
-            ScanInfo scanInformation = virusTotalRef.scanFile(new File(filePath));
+            ScanInfo scanInformation = virusTotalRef.scanFile(fileToScan);
 
-            System.out.println("___SCAN INFORMATION___");
-            System.out.println("MD5 :\t" + scanInformation.getMd5());
-            System.out.println("Perma Link :\t" + scanInformation.getPermalink());
-            System.out.println("Resource :\t" + scanInformation.getResource());
-            System.out.println("Scan Date :\t" + scanInformation.getScanDate());
-            System.out.println("Scan Id :\t" + scanInformation.getScanId());
-            System.out.println("SHA1 :\t" + scanInformation.getSha1());
-            System.out.println("SHA256 :\t" + scanInformation.getSha256());
-            System.out.println("Verbose Msg :\t" + scanInformation.getVerboseMessage());
-            System.out.println("Response Code :\t" + scanInformation.getResponseCode());
-            System.out.println("done.");
+            Log.d("Scan file", "___SCAN INFORMATION___");
+            Log.d("Scan file", "MD5 :\t" + scanInformation.getMd5());
+            Log.d("Scan file", "Perma Link :\t" + scanInformation.getPermalink());
+            Log.d("Scan file", "Resource :\t" + scanInformation.getResource());
+            Log.d("Scan file", "Scan Date :\t" + scanInformation.getScanDate());
+            Log.d("Scan file", "Scan Id :\t" + scanInformation.getScanId());
+            Log.d("Scan file", "SHA1 :\t" + scanInformation.getSha1());
+            Log.d("Scan file", "SHA256 :\t" + scanInformation.getSha256());
+            Log.d("Scan file", "Verbose Msg :\t" + scanInformation.getVerboseMessage());
+            Log.d("Scan file", "Response Code :\t" + scanInformation.getResponseCode());
+            Log.d("Scan file", "done.");
+            
+            return scanInformation.getResource();
         } catch (APIKeyNotFoundException ex) {
-            System.err.println("API Key not found! " + ex.getMessage());
+            Log.d("Error", "1");
         } catch (UnsupportedEncodingException ex) {
-            System.err.println("Unsupported Encoding Format!" + ex.getMessage());
+            Log.d("Error", "2");
         } catch (UnauthorizedAccessException ex) {
-            System.err.println("Invalid API Key " + ex.getMessage());
+            Log.d("Error", "3");
         } catch (Exception ex) {
-            System.err.println("Something Bad Happened! " + ex.getMessage());
+            Log.d("Error","4");
+        }
+        return null;
+    }
+
+    public static void getFileScanReport(String resource) {
+        try {
+            VirusTotalConfig.getConfigInstance().setVirusTotalAPIKey("4ae0176f319cdb95bc384f4c85b7679d1a28f391e4d47f482a4b67f26a487bc9");
+            VirustotalPublicV2 virusTotalRef = new VirustotalPublicV2Impl();
+            
+            FileScanReport report = virusTotalRef.getScanReport(resource);
+
+            Log.d("Scan file", "Positives :\t" + report.getPositives());
+
+        } catch (APIKeyNotFoundException ex) {
+            Log.d("Scan file", "API Key not found! " + ex.getMessage());
+        } catch (UnsupportedEncodingException ex) {
+            Log.d("Scan file", "Unsupported Encoding Format!" + ex.getMessage());
+        } catch (UnauthorizedAccessException ex) {
+            Log.d("Scan file", "Invalid API Key " + ex.getMessage());
+        } catch (Exception ex) {
+            Log.d("Scan file", "Something Bad Happened! " + ex.getMessage());
         }
     }
 }
--- app/src/main/java/com/example/xender/activity/MainActivity.java
+++ app/src/main/java/com/example/xender/activity/MainActivity.java
@@ -2,26 +2,39 @@ package com.example.xender.activity;
 
 import static androidx.core.app.NotificationCompat.EXTRA_NOTIFICATION_ID;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.FileProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -32,9 +45,19 @@ import android.widget.Toast;
 
 import com.example.xender.Dialog.TransferDialog;
 import com.example.xender.R;
+import com.example.xender.Utils.VirusScanUtil;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> activityResultLauncher;
     NavController navController;
     Toolbar toolbar;
     public static boolean downloading = true;
@@ -42,59 +65,117 @@ public class MainActivity extends AppCompatActivity {
     int PROGRESS_CURRENT = 0;
     int PROGRESS_STEP = 5;
     Thread updateProgress;
-    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    String resource;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        createNotificationChannel();
-        Button showDialog = findViewById(R.id.ShowDialog);
-        showDialog.setOnClickListener(v -> {
-            Intent snoozeIntent = new Intent(this, MyBroadcastReceiver.class);
-            snoozeIntent.setAction(MyBroadcastReceiver.ACTION_CANCEL);
-            PendingIntent snoozePendingIntent =
-                    PendingIntent.getBroadcast(this, 0, snoozeIntent, PendingIntent.FLAG_IMMUTABLE);
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        Uri returnUri = data.getData();
 
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Transfer")
-                    .setSmallIcon(R.drawable.send_svgrepo_com)
-                    .setContentTitle("Transferring files...")
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                    .addAction(R.drawable.send_svgrepo_com, "Cancel", snoozePendingIntent);
+                        String name = getFileNameFromUri(this, returnUri);
 
-            builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
-            NotificationManagerCompat notification = NotificationManagerCompat.from(this);
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                Log.d("Permission", "Failed");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
-                return;
-            }
-
-            updateProgress = new Thread(() -> {
-                notification.notify(1, builder.build());
-
-                while (downloading && PROGRESS_CURRENT < PROGRESS_MAX) {
-                    PROGRESS_CURRENT += PROGRESS_STEP;
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+                        File scanFile = new File(getFilesDir().getAbsolutePath(), "haha.txt");
+                        Log.d("File exist", scanFile.exists() ? "True" : "False");
+                        Log.d("File name", scanFile.getName());
+                        Log.d("File path", scanFile.getPath());
+                        VirusScanUtil.scanFile(scanFile);
+//                        try (InputStream inputStream = getContentResolver().openInputStream(data.getData())) {
+//                            File scanFile = new File(getFilesDir().getAbsolutePath(), name);
+//                            Log.d("File name", scanFile.getName());
+//                            Log.d("File path", scanFile.getPath());
+//                            FileOutputStream outputStream = new FileOutputStream(scanFile);
+//
+//                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//                                FileUtils.copy(inputStream, outputStream);
+//                            }
+//                            outputStream.close();
+//                        } catch (IOException e) {
+//                            throw new RuntimeException(e);
+//                        }
                     }
-                    builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
-                    notification.notify(1, builder.build());
+                });
 
-                    if (PROGRESS_CURRENT > PROGRESS_MAX)
-                        PROGRESS_CURRENT = PROGRESS_MAX;
-                }
-
-                builder.setContentText("Download complete")
-                        .setProgress(0,0,false);
-                notification.notify(1, builder.build());
+        Button chooseFile = findViewById(R.id.ShowDialog);
+        chooseFile.setOnClickListener(v -> {
+            File scanFile = new File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "Use-case.txt");
+            Log.d("File readable", scanFile.canRead() ? "True" : "False");
+            Log.d("File exist", scanFile.exists() ? "True" : "False");
+            Log.d("File name", scanFile.getName());
+            Log.d("File path", scanFile.getPath());
+            Log.d("File size", String.valueOf(scanFile.length()));
+            Thread scanVirusThread = new Thread(() -> {
+                resource = VirusScanUtil.scanFile(scanFile);
             });
-
-            updateProgress.start();
+            scanVirusThread.start();
+//            Intent filechooser = new Intent(Intent.ACTION_GET_CONTENT);
+//            filechooser.setType("*/*");
+//            filechooser.addCategory(Intent.CATEGORY_OPENABLE);
+//            activityResultLauncher.launch(filechooser);
         });
+
+        Button getScanInfo = findViewById(R.id.GetScan);
+        getScanInfo.setOnClickListener(v -> {
+            Thread getScanReport = new Thread(() -> {
+                VirusScanUtil.getFileScanReport(resource);
+            });
+            getScanReport.start();
+        });
+//        createNotificationChannel();
+//        Button showDialog = findViewById(R.id.ShowDialog);
+//        showDialog.setOnClickListener(v -> {
+//            Intent snoozeIntent = new Intent(this, MyBroadcastReceiver.class);
+//            snoozeIntent.setAction(MyBroadcastReceiver.ACTION_CANCEL);
+//            PendingIntent snoozePendingIntent =
+//                    PendingIntent.getBroadcast(this, 0, snoozeIntent, PendingIntent.FLAG_IMMUTABLE);
+//
+//            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "Transfer")
+//                    .setSmallIcon(R.drawable.send_svgrepo_com)
+//                    .setContentTitle("Transferring files...")
+//                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                    .addAction(R.drawable.send_svgrepo_com, "Cancel", snoozePendingIntent);
+//
+//            builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
+//            NotificationManagerCompat notification = NotificationManagerCompat.from(this);
+//
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+//                Log.d("Permission", "Failed");
+//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+//                return;
+//            }
+//
+//            updateProgress = new Thread(() -> {
+//                notification.notify(1, builder.build());
+//
+//                while (downloading && PROGRESS_CURRENT < PROGRESS_MAX) {
+//                    PROGRESS_CURRENT += PROGRESS_STEP;
+//                    try {
+//                        Thread.sleep(500);
+//                    } catch (InterruptedException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//                    builder.setProgress(PROGRESS_MAX, PROGRESS_CURRENT, false);
+//                    notification.notify(1, builder.build());
+//
+//                    if (PROGRESS_CURRENT > PROGRESS_MAX)
+//                        PROGRESS_CURRENT = PROGRESS_MAX;
+//                }
+//
+//                builder.setContentText("Download complete")
+//                        .setProgress(0,0,false);
+//                notification.notify(1, builder.build());
+//            });
+//
+//            updateProgress.start();
+//        });
+
+        
 //        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //
 //        toolbar = findViewById(R.id.appbar_main);
@@ -118,5 +199,15 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
+    }
+
+    private String getFileNameFromUri(Context context, Uri uri) {
+        Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+        Log.d("Get name", "Ok");
+        cursor.moveToFirst();
+        int index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        String fileName = cursor.getString(index);
+        cursor.close();
+        return fileName;
     }
 }
